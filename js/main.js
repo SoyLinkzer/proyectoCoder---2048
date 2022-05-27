9b3108e7ef52d4047fd5fa2d0a85c05aeac2f8d2
SoyLinkzer <macabral@frba.utn.edu.ar>
2022-05-27 20:32:15 -0300

    v0.2

--- js/main.js
+++ js/main.js
@@ -16,17 +16,19 @@ class casilla {
   
 //defino el tamaño de la matriz
 const LADO = 4;
+let puntuacion = 0;
 
-let pantalla = document.getElementById("pantalla");
+let pantallaHTML = document.getElementById("pantalla");
+let puntuacionHTML = document.getElementById("puntuacion");
 
 //inicializar matriz de casillas
 let casillas = [];
 for(let i = 0; i<LADO; i++){
     casillas.push([]);
     for(let j = 0; j<LADO; j++){
-        casillas[i].push(new casilla(2,[i,j],document.createElement("div")));
+        casillas[i].push(new casilla(0,[i,j],document.createElement("div")));
         //casillas[i].push(new casilla(i+"."+j,[i,j],document.createElement("div")));
-        casillas[i][j].dibujarse(pantalla,'casilla');
+        casillas[i][j].dibujarse(pantallaHTML,'casilla');
     }
 }
 
@@ -116,6 +118,55 @@ const filasAColumnas = (matrizATransformar)=>{
     return matrizAux;
 };
 
+const calcularPuntuacion = (matrizASumar)=>{
+    //revisa el total de las casillas, determina la puntuacio, y la actualiza en el html
+    let total = 0;
+    matrizASumar.forEach((fila=>{
+        fila.forEach(columna=>{
+            total += columna.valor;
+        })
+    }));
+    return total;
+}
+
+const actualizarPuntuacion = (matriz,elementoHTML)=>{
+    elementoHTML.innerText = `Puntuacion: ${calcularPuntuacion(matriz)}`;
+};
+
+const getRandomInt = (min, max)=>{
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const aparecerRandom = (matriz)=>{
+    let arrayAux; 
+    let casillaAux;
+    //Obtengo solo filas con valores = 0
+    matriz = matriz.map(fila=>{
+        return fila.filter(columna=>(columna.valor === 0));
+    });
+    matriz += matriz.forEach(fila=>{
+        return 
+    });
+    console.log(matriz);
+    if(matriz != [[],[],[],[]]){
+        arrayAux = matriz[getRandomInt(0,matriz.length)];
+        casillaAux = arrayAux[getRandomInt(0,arrayAux.length)];
+        console.log(casillaAux);
+        console.log(casillaAux.valor);
+        casillaAux.valor = 2;
+        casillaAux.actualizarCasilla();
+        console.log(casillaAux.posicion);
+    }else{
+        alert("Perdiste :c");
+    }
+};
+
+const actualizarTotal = (matrizAActualizar,elementoHTML)=>{
+    actualizarCasillasHTML(matrizAActualizar);
+    actualizarPuntuacion(matrizAActualizar,elementoHTML);
+    aparecerRandom(matrizAActualizar);
+};
+
 //Algunas pruebas
 //let matrizDePrueba = [[1,2,3,4],[4,3,2,1],[5,6,7,8],[8,7,6,5]];
 //console.log(filasAColumnas(matrizDePrueba));
@@ -124,26 +175,28 @@ const filasAColumnas = (matrizATransformar)=>{
 
 let casillasNumAux;
 
+aparecerRandom(casillas);
+
 document.addEventListener('keydown',(t)=>{
     switch(t.key){
         case 'ArrowUp':
             casillasNumAux = moverCasillas(filasAColumnas(mObjetosToMNumeros(casillas)));
             casillas = actualizarValores(casillas,filasAColumnas(casillasNumAux));
-            actualizarCasillasHTML(casillas);
+            actualizarTotal(casillas,puntuacionHTML);
             break;
         case 'ArrowDown':
             casillasNumAux = moverCasillas(revertirMatriz(filasAColumnas(mObjetosToMNumeros(casillas))));
             casillas = actualizarValores(casillas,filasAColumnas(revertirMatriz(casillasNumAux)));
-            actualizarCasillasHTML(casillas);
+            actualizarTotal(casillas,puntuacionHTML);
             break;
         case 'ArrowLeft':
             casillas = actualizarValores(casillas,moverCasillas(mObjetosToMNumeros(casillas)));
-            actualizarCasillasHTML(casillas);
+            actualizarTotal(casillas,puntuacionHTML);
             break;
         case 'ArrowRight':
             casillasNumAux = moverCasillas(revertirMatriz(mObjetosToMNumeros(casillas)));
             casillas = actualizarValores(casillas,revertirMatriz(casillasNumAux));
-            actualizarCasillasHTML(casillas);
+            actualizarTotal(casillas,puntuacionHTML);
             break;
         default:
             break;
